afa99979785cae7a1e0f9a861e2840a58dd9b39a
Svyatoslav <jana.polya@yandex.ru>
2024-08-09 15:32:34 +0300

    +

--- js/sticky-header.js
+++ js/sticky-header.js
@@ -3,11 +3,19 @@ let ticking = false;
 
 function headerChange() {
   let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-  if (scrollTop > 126) {
+  document.body.classList.add("header-sticky");
+  if (scrollTop > 200) {
+    let img = document.querySelector('.hero__block-img');
+    img.style.zIndex = 1;
+  }
+  if (scrollTop > 125) {
     document.body.classList.add("header-sticky");
+
+
   } else {
     document.body.classList.remove("header-sticky");
+    let img = document.querySelector('.hero__block-img');
+    img.style.zIndex = 11;
   }
 
   ticking = false;
